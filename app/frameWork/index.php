687113616676f9fab146231acb924f33da51f847
projetointegradorunivesp2025 <projetointegradorunivesp65@gmail.com>
2025-04-12 16:17:39 -0300

    Update index.php

--- app/frameWork/index.php
+++ app/frameWork/index.php
@@ -1,1 +1,13 @@
 
+/**
+ * app/framework/index.php
+ *
+ * @version    1.0
+ * @package    control
+ * @subpackage admin
+ * @author     Grupo Projeto Integrador UNIVESP
+ * @copyright  Copyright (c) 2025 
+ * @license    Licença Pública Geral GNU (GPL3)
+ */
+
+header("Location: /projetos/UNIVESP-PI3/");
