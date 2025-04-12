ecb5af7536535d5e5bd6b8d80e18191bb974b761
projetointegradorunivesp2025 <projetointegradorunivesp65@gmail.com>
2025-04-12 16:08:10 -0300

    Add files via upload

---
+++ privacidade.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * privacidade.php
+ *
+ * @version    1.0
+ * @package    control
+ * @subpackage admin
+ * @author     Grupo Projeto Integrador UNIVESP
+ * @copyright  Copyright (c) 2025 
+ * @license    Licença Pública Geral GNU (GPL3)
+ */
+
+echo "Privacidade";
